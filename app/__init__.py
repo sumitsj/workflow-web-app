725907ef6c75c5269b727af089dea390e934679a
Sumit Jadhav <summit.jadhav@gmail.com>
2018-12-22 20:30:31 +0530

    Restructure project.

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,30 @@
 import os
 from flask import Flask
-from app import health, workflow, auth
 from flask_assets import Environment, Bundle
-
-from .database import db
-from .errors import *
 
 
 def create():
     app = Flask(__name__)
     app.config.from_object(os.getenv('FLASK_CONFIG', 'config.DevelopmentConfig'))
 
+    # initialise database
+    from .database import db
     with app.app_context():
         db.init_app(app)
         db.create_all()
 
     # apply blueprints to the app
+    from app.blueprints import auth
+    from app.blueprints import health
+    from app.blueprints import workflow
     app.register_blueprint(health.blueprint)
     app.register_blueprint(workflow.blueprint)
     app.register_blueprint(auth.blueprint)
 
     # register error handlers
+    from .errors import internal_error
+    from .errors import not_found_error
+    from .errors import handle_bad_request
     app.register_error_handler(400, handle_bad_request)
     app.register_error_handler(404, not_found_error)
     app.register_error_handler(500, internal_error)
--- app/blueprints/health.py
+++ app/blueprints/health.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from .database import db
+from app.database import db
 
 blueprint = Blueprint('health', __name__, url_prefix='/health')
 
